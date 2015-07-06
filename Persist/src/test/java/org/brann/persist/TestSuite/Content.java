c7fa9a9fc1ad45ed17ef44c3326a690a342926fd
John Brann <jbrann@panix.com>
2015-07-06 16:21:23 +0100

    commit test suite

---
+++ Persist/src/test/java/org/brann/persist/TestSuite/Content.java
@@ -0,0 +1,31 @@
+package org.brann.persist.TestSuite;
+
+@SuppressWarnings("serial")
+public class Content implements java.io.Serializable {
+    
+    private int value;
+    
+    public Content() {
+        value = 0;
+    }
+    
+    /** Getter for property value.
+     * @return Value of property value.
+     */
+    public int getValue() {
+        return value;
+    }
+    
+    /** Setter for property value.
+     * @param value New value of property value.
+     */
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    public String toString() {
+        return Integer.toString(value);
+    }
+    
+}
+
